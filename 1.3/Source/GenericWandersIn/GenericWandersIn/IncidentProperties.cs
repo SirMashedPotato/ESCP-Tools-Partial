50627f738d9bd048feaaa618d1abda0dda2274ab
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-04-03 10:09:21 +1200

    Generic wanders in

---
+++ 1.3/Source/GenericWandersIn/GenericWandersIn/IncidentProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using RimWorld;
+
+namespace GenericWandersIn
+{
+    class IncidentProperties : DefModExtension
+    {
+        public PawnKindDef kindDef;
+        public IntRange max = new IntRange(3, 5);
+        public bool leaveMapAfterTime = true;
+
+        public static IncidentProperties Get(Def def)
+        {
+            return def.GetModExtension<IncidentProperties>();
+        }
+    }
+}
---
+++ 1.3/Source/GenericWandersIn/GenericWandersIn/IncidentWorker_GenericWandersIn.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Reflection.Emit;
+using System.Linq;
+
+
+namespace GenericWandersIn
+{
+	class IncidentWorker_GenericWandersIn : IncidentWorker
+	{
+		protected override bool CanFireNowSub(IncidentParms parms)
+		{
+			var props = IncidentProperties.Get(this.def);
+			if (!base.CanFireNowSub(parms) && props != null && props.kindDef != null)
+			{
+				return false;
+			}
+			Map map = (Map)parms.target;
+			IntVec3 intVec;
+			return !map.gameConditionManager.ConditionIsActive(GameConditionDefOf.ToxicFallout) && map.mapTemperature.SeasonAndOutdoorTemperatureAcceptableFor(props.kindDef.race) && this.TryFindEntryCell(map, out intVec);
+		}
+
+		protected override bool TryExecuteWorker(IncidentParms parms)
+		{
+			var props = IncidentProperties.Get(this.def);
+			Map map = (Map)parms.target;
+			IntVec3 intVec;
+			if (!this.TryFindEntryCell(map, out intVec))
+			{
+				return false;
+			}
+			int num = GenMath.RoundRandom(StorytellerUtility.DefaultThreatPointsNow(map) / props.kindDef.combatPower);
+			int max = props.max.RandomInRange;
+			num = Mathf.Clamp(num, 1, max);
+			int num2 = Rand.RangeInclusive(90000, 150000);
+			IntVec3 invalid = IntVec3.Invalid;
+			if (!RCellFinder.TryFindRandomCellOutsideColonyNearTheCenterOfTheMap(intVec, map, 10f, out invalid))
+			{
+				invalid = IntVec3.Invalid;
+			}
+			Pawn pawn = null;
+			for (int i = 0; i < num; i++)
+			{
+				IntVec3 loc = CellFinder.RandomClosewalkCellNear(intVec, map, 10, null);
+				pawn = PawnGenerator.GeneratePawn(props.kindDef, null);
+				GenSpawn.Spawn(pawn, loc, map, Rot4.Random, WipeMode.Vanish, false);
+				if (props.leaveMapAfterTime)
+				{
+					pawn.mindState.exitMapAfterTick = Find.TickManager.TicksGame + num2;
+				}
+				if (invalid.IsValid)
+				{
+					pawn.mindState.forcedGotoPosition = CellFinder.RandomClosewalkCellNear(invalid, map, 10, null);
+				}
+			}
+			Find.LetterStack.ReceiveLetter(this.def.letterLabel.Formatted(props.kindDef.label).CapitalizeFirst(), this.def.letterText.Formatted(num, props.kindDef.label), this.def.letterDef, pawn, null, null);
+			return true;
+		}
+
+		private bool TryFindEntryCell(Map map, out IntVec3 cell)
+		{
+			return RCellFinder.TryFindRandomPawnEntryCell(out cell, map, CellFinder.EdgeRoadChance_Animal + 0.2f, false, null);
+		}
+	}
+}
+
